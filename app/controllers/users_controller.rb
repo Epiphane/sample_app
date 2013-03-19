f664096d26fce77114cbca7fefd90f5f1b59ea0e
Thomas Steinke <exyphnos@gmail.com>
2013-03-19 13:14:09 -0700

    Initial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :not_signed_in, only: [:new, :create]
   before_filter :correct_user, only: [:edit, :update]
   before_filter :admin_user, only: :destroy
   
@@ -71,5 +72,9 @@ class UsersController < ApplicationController
     
     def admin_user
       redirect_to(root_path) unless current_user.admin?
+    end
+    
+    def not_signed_in
+      redirect_to root_url, unless !signed_in?
     end
 end
